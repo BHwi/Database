1939d37c7271d21c9b49d3564f477f757c070d27
BHwi <43560497+BHwi@users.noreply.github.com>
2021-04-14 20:39:14 +0900

    210414 study upload

---
+++ 02_data_management/DB_02_01_09.sql
@@ -0,0 +1,12 @@
+USE course_rating;
+
+-- INSERT INTO student
+-- 	(id, name, student_number, major, email, phone, admission_date)
+--     VALUES 
+--     (1, '성태후', 20142947, '컴퓨터공학과', 'taehos@naver.com', '010-1234-5678', '2014-03-12');
+
+# 모든 column을 추가할 경우 따로 앞에 명시하지 않아도 추가할 수 있음.
+INSERT INTO student
+    VALUES 
+    (2, '김소원', 20130912, '화학과', 'sungso@naver.com', '010-2345-6789', '2013-03-07');
+    
---
+++ 02_data_management/DB_02_01_10.sql
@@ -0,0 +1,6 @@
+USE course_rating;
+
+INSERT INTO student
+	(id, name, student_number, major, admission_date)
+    VALUES 
+    (3, '이현승', 2011025, '법학과', '2014-03-12');
